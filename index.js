8b04e42ce2b7cf85869fb75ddedead0d699e2c20
Clara(신상림) <sangrim.shin@dabeeo.com>
2022-11-30 16:07:45 +0900

    mapContainer가 여러개되는 이슈 해결

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { mapList } from './qaTest/mapList.js';
 import { menuList } from './menuList.js';
 
 const dabeeoMaps = new dabeeo.Maps();
-const mapContainer = document.querySelector('.map');
+// let gui = null;
 const gui = new dat.GUI({ width: 300, right: 0, autoPlace: false });
 document.querySelector('.guiContainer').append(gui.domElement);
 
@@ -18,26 +18,11 @@ async function init() {
     // nav menu를 만든다
     makeNavMenu(menuList);
 
-    // mapData를 fetch하여 rendering한후 메뉴를 생성한다
-    await getMapDataByIndex(mapIndex);
+    //초기 메뉴를 만든다
+    await initMenu();
 
     //nav bar active class switching
-    document.querySelector('.nav').addEventListener('click', (e) => {
-        // nav 메뉴를 클릭하지 않았거나 이미 active되어 있는 메뉴를 클릭했으면 return
-        if (e.target.parentElement.className !== 'nav' || e.target.classList.contains('active')) return;
-
-        // 현재 메뉴를 숨긴다
-        currentMenu.hide();
-
-        //active class에서 active를 제거한다
-        e.target.parentElement.querySelector('li.active').classList.remove('active');
-
-        //현재 선택된 element에 active를 붙인다
-        e.target.classList.add('active');
-
-        //현재 선택된 메뉴를 보여준다
-        currentMenu = activateMenu(e.target.getAttribute('name'));
-    });
+    document.querySelector('.nav').addEventListener('click', menuSwitch);
 }
 
 // navigation bar를 생성한다
@@ -50,6 +35,91 @@ function makeNavMenu(menuList) {
         nav.appendChild(li);
     });
     nav.querySelector('[name="getMap"]').classList.add('active');
+}
+
+function makeMapElement() {
+    // map 그리기
+    const section = document.querySelector('.section');
+    const mapContainer = document.createElement('div');
+    mapContainer.className = 'map';
+    section.appendChild(mapContainer);
+    return mapContainer;
+}
+// 최초로 메뉴를 만든다
+async function initMenu() {
+    // mapData를 fetch
+    mapData = await dabeeoMaps.getMapData({
+        clientId: mapList[0].clientId,
+        clientSecret: mapList[0].clientSecret,
+        serverType: 'SERVER_REAL',
+    });
+
+    // mapContainer를 만든다
+    const mapContainer = makeMapElement();
+
+    // map을 그린다
+    map = await dabeeoMaps.showMap(mapContainer, {}, mapData);
+
+    // 메뉴를 만든다
+    getMapMenu = initGetMap(gui);
+    mapOptionMenu = initMapOptionMenu(gui);
+    mapOptionMenu.hide();
+    floorMenu = initFloorMenu(gui, mapData, map, mapContainer);
+    menuList.forEach(async (element) => {
+        if (element.menu) {
+            element.instance = new element.menu();
+            element.init = await element.instance.init(gui, mapData, map, mapContainer);
+            element.init.hide();
+        }
+    });
+    console.log(menuList);
+
+    currentMenu = getMapMenu;
+}
+
+function initGetMap(parentMenu) {
+    const menu = parentMenu.addFolder('getMap Menu ');
+    menu.open();
+    initGetMapByInput(menu);
+    initGetMapByList(menu);
+    return menu;
+}
+function initGetMapByInput(parentMenu) {
+    const menu = parentMenu.addFolder('get Map by client info ');
+
+    const setting = {
+        clientId: '',
+        clientSecret: '',
+        serverType: 'SERVER_REAL',
+        getMapByInput: getMapByInput,
+    };
+    menu.add(setting, 'clientId');
+    menu.add(setting, 'clientSecret');
+    menu.add(setting, 'serverType', ['SERVER_REAL', 'SERVER_STAGE']);
+    menu.add(setting, 'getMapByInput');
+
+    async function getMapByInput() {
+        const option = {
+            clientId: setting.clientId,
+            clientSecret: setting.clientSecret,
+            serverType: setting.serverType,
+        };
+        await getMapData(option);
+    }
+}
+
+function initGetMapByList(menu) {
+    let mapSetting = { 선택: -1 };
+    mapList.forEach((element, index) => {
+        mapSetting[element.name] = index;
+    });
+
+    const setting = { mapIndex: mapIndex };
+    menu.add(setting, 'mapIndex', mapSetting).onChange(initGetMapDataByIndex);
+    async function initGetMapDataByIndex(value) {
+        mapIndex = value;
+        await getMapDataByIndex(value);
+    }
 }
 
 // mapList의 0번째 지도를 가져온다
@@ -67,45 +137,86 @@ async function getMapDataByIndex(index) {
 }
 
 async function getMapData(option) {
+    // 맵 데이터 새로 가져오기
     mapData = await dabeeoMaps.getMapData(option);
+
+    //이전에 그려졌던 맵 삭제 및 메모리 해제
     if (map) map.context.cleanup();
+    document.querySelector('.map').remove();
+
+    //맵 컨테이너 만들고 맵 그리기
+    const mapContainer = makeMapElement();
     map = await dabeeoMaps.showMap(mapContainer, {}, mapData);
-    if (floorMenu) gui.remove(floorMenu);
-    gui.destroy();
+
+    //메뉴 삭제
+    gui.removeFolder(mapOptionMenu);
+    removeAllMenu();
+
+    // 메뉴 다시 만들기
+    mapOptionMenu = initMapOptionMenu(gui);
+    mapOptionMenu.hide();
+
     initAllMenu(gui, mapData, map, mapContainer);
 }
 
-function activateMenu(menuName) {
-    let currentMenu = null;
-    if (menuName === 'getMap') {
-        getMapMenu.show();
-        currentMenu = getMapMenu;
-    } else if (menuName === 'mapOption') {
-        mapOptionMenu.show();
-        currentMenu = mapOptionMenu;
-    } else {
-        const menuFound = menuList.find((element) => element.name === menuName);
-        menuFound.init.show();
-        currentMenu = menuFound.init;
-    }
-    return currentMenu;
+// 메뉴 삭제
+function removeAllMenu() {
+    gui.removeFolder(floorMenu);
+    menuList.forEach(async (element) => {
+        if (element.menu) {
+            element.instance.removeMenu();
+        }
+    });
 }
+
+// 메뉴 다시 만들기
 function initAllMenu(parentMenu, mapData, map, mapContainer) {
-    getMapMenu = initGetMap(parentMenu);
     floorMenu = initFloorMenu(parentMenu, mapData, map, mapContainer);
     menuList.forEach(async (element) => {
         if (element.menu) {
-            element.init = await new element.menu().init(gui, mapData, map, mapContainer);
+            element.init = await element.instance.init(gui, mapData, map, mapContainer);
             element.init.hide();
         }
     });
     console.log(menuList);
-    mapOptionMenu = initMapOptionMenu(parentMenu);
-    mapOptionMenu.hide();
 
     currentMenu = getMapMenu;
 }
+
+function menuSwitch(e) {
+    // nav 메뉴를 클릭하지 않았거나 이미 active되어 있는 메뉴를 클릭했으면 return
+    if (e.target.parentElement.className !== 'nav' || e.target.classList.contains('active')) return;
+
+    //active class에서 active를 제거한다
+    e.target.parentElement.querySelector('li.active').classList.remove('active');
+
+    //현재 선택된 element에 active를 붙인다
+    e.target.classList.add('active');
+
+    // 현재 메뉴를 숨긴다
+    currentMenu.hide();
+
+    //현재 선택된 메뉴를 보여준다
+    currentMenu = activateMenu(e.target.getAttribute('name'));
+}
+
+function activateMenu(menuName) {
+    if (menuName === 'getMap') {
+        getMapMenu.show();
+        return getMapMenu;
+    } else if (menuName === 'mapOption') {
+        mapOptionMenu.show();
+        return mapOptionMenu;
+    } else {
+        const menuFound = menuList.find((element) => element.name === menuName);
+        menuFound.init.show();
+        return menuFound.init;
+    }
+}
+
 function initFloorMenu(gui, mapData, map, mapContainer) {
+    const menu = gui.addFolder('Floor');
+    menu.open();
     mapContainer.addEventListener('floor-changed', (e) => {
         console.log('floor-changed 에 대한 결과값', e.detail);
         setting.floor = e.detail.id;
@@ -124,71 +235,31 @@ function initFloorMenu(gui, mapData, map, mapContainer) {
     const setting = {
         changeFloor: currentFloor.id,
     };
-    return gui.add(setting, 'changeFloor', floorList).onChange(changeFloor).listen();
-}
-
-function initGetMap(parentMenu) {
-    const menu = parentMenu.addFolder('getMap Menu ');
-    menu.open();
-    initGetMapByInput(menu);
-    initGetMapByList(menu);
+    menu.add(setting, 'changeFloor', floorList).onChange(changeFloor).listen();
     return menu;
-}
-
-function initGetMapByInput(parentMenu) {
-    const setting = {
-        clientId: '',
-        clientSecret: '',
-        serverType: 'SERVER_REAL',
-        getMap: getMap,
-    };
-    const menu = parentMenu.addFolder('get Map by client info ');
-    menu.add(setting, 'clientId');
-    menu.add(setting, 'clientSecret');
-    menu.add(setting, 'serverType', ['SERVER_REAL', 'SERVER_STAGE']);
-    menu.add(setting, 'getMap');
-
-    async function getMap() {
-        const option = {
-            clientId: setting.clientId,
-            clientSecret: setting.clientSecret,
-            serverType: setting.serverType,
-        };
-        await getMapData(option);
-    }
-    return menu;
-}
-
-function initGetMapByList(parentMenu) {
-    let mapSetting = { 선택: -1 };
-    mapList.forEach((element, index) => {
-        mapSetting[element.name] = index;
-    });
-
-    const setting = { mapIndex: mapIndex };
-    parentMenu.add(setting, 'mapIndex', mapSetting).onChange(initGetMapDataByIndex);
-    async function initGetMapDataByIndex(value) {
-        mapIndex = value;
-        await getMapDataByIndex(value);
-    }
 }
 
 function initMapOptionMenu(parentMenu) {
-    const setting = initSetting();
-    const menu = initMenu(setting, parentMenu);
+    const setting = initOptionSetting();
+    const menu = initOptionMenu(setting, parentMenu);
     const actionSetting = {
         showMap: showMap,
     };
     menu.add(actionSetting, 'showMap');
+
     async function showMap() {
-        const option = getOption(setting);
         if (map) map.context.cleanup();
+        document.querySelector('.map').remove();
+
+        const option = getOption(setting);
+        const mapContainer = makeMapElement();
         map = await dabeeoMaps.showMap(mapContainer, option, mapData);
-        gui.remove(floorMenu);
-        gui.destroy();
+
+        removeAllMenu();
+
         initAllMenu(gui, mapData, map, mapContainer);
-        getMapMenu.hide();
-        mapOptionMenu.show();
+        // getMapMenu.hide();
+        // mapOptionMenu.show();
         currentMenu = mapOptionMenu;
     }
     function getOption(setting) {
@@ -219,7 +290,7 @@ function initMapOptionMenu(parentMenu) {
     return menu;
 }
 
-function initSetting() {
+function initOptionSetting() {
     const defaultFloorId = mapData.dataFloor.getDefaultFloor().id;
     const defaultLang = mapData.dataLanguage.getDefaultLanguage().lang;
 
@@ -242,13 +313,13 @@ function initSetting() {
     return setting;
 }
 
-function initMenu(setting, parentMenu) {
+function initOptionMenu(setting, parentMenu) {
+    const menu = parentMenu.addFolder('mapOption');
     const floorSetting = mapData.dataFloor.getFloors().reduce((prev, cur) => {
         return { ...prev, [cur.name[0].text]: cur.id };
     }, {});
     const langSetting = mapData.dataLanguage.getLanguage().map((data) => data.lang);
 
-    const menu = parentMenu.addFolder('mapOption');
     menu.open();
     menu.add(setting, 'camera', ['2d', '3d']);
     menu.add(setting, 'floor', floorSetting);
