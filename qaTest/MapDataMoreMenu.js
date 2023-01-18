3c549610617b7fbf087a7b84b156a5783a1764cd
Daniel_hyun <byunghyun.oh@dabeeo.com>
2023-01-18 18:15:37 +0900

    사무실용 데이터 테스트 추가

--- qaTest/MapDataMoreMenu.js
+++ qaTest/MapDataMoreMenu.js
@@ -14,6 +14,10 @@ export class MapDataMoreMenu {
         this.mapContainer = mapContainer;
         this.addmenu = this.gui.addFolder('More_사무실용');
         this.initFloorData(this.addmenu);
+        this.initLangData(this.addmenu);
+        this.initPoiData(this.addmenu);
+        this.initObjData(this.addmenu);
+        this.initGroupData(this.addmenu);
         return this.addmenu;
     }
 
@@ -30,5 +34,68 @@ export class MapDataMoreMenu {
             FloorData: FloorData,
         };
         gui.add(setting, 'FloorData');
+    }
+
+    initLangData(gui) {
+        const LangData = () => {
+            console.log('---------------');
+            console.log('Language Info: ', this.mapData.dataLanguage.getLanguage()); // 현재 지도의 모든 language 정보 반환
+            console.log('default language: ', this.mapData.dataLanguage.getDefaultLanguage()); // 현재 지도에 적용된 language 정보 반환
+            console.log('---------------');
+        };
+        const setting = {
+            LangData: LangData,
+        };
+        gui.add(setting, 'LangData');
+    }
+
+    initPoiData(gui) {
+        const PoiData = () => {
+            console.log('---------------');
+            console.log('poi data: ', this.mapData.dataPoi.getPois()); // poi data 가져오기
+            console.log('find poi by title: ', this.mapData.dataPoi.find({ title: '실' })); // 1. title로 찾을 경우
+            console.log('find poi by id: ', this.mapData.dataPoi.find({ id: 'PO-9InVzIGv20417' })); // 2. id로 찾을 경우
+            console.log('find poi by floorId: ', this.mapData.dataPoi.find({ floorId: 'FL-t4vqgyek3jnb8146' })); // 3. floorId로 찾을 경우
+            console.log('find poi by groupCode: ', this.mapData.dataPoi.find({ groupCode: 'A1-12' })); // 4. groupCode로 찾을 경우
+            console.log('---------------');
+        };
+        const setting = {
+            PoiData: PoiData,
+        };
+        gui.add(setting, 'PoiData');
+    }
+
+    initObjData(gui) {
+        const ObjData = () => {
+            console.log('---------------');
+            console.log('object data ', this.mapData.dataObject.getObjects('FL-t4vqgyek3jnb8146'));
+            console.log('find object by title: ', this.mapData.dataObject.find({ title: '실' })); // 1. title로 찾을 경우
+            console.log('find object by id: ', this.mapData.dataObject.find({ id: 'OB-aN7fGeVoze1959' })); // 2. id로 찾을 경우
+            console.log('find object by floorId: ', this.mapData.dataObject.find({ floorId: 'FL-t4vqgyek3jnb8146' })); // 3. floorId로 찾을 경우
+            console.log('find object by GroupCode: ', this.mapData.dataObject.find({ groupCode: 'A1-1' })); // 4. groupCode로 찾을 경우
+            console.log('---------------');
+        };
+        const setting = {
+            ObjData: ObjData,
+        };
+        gui.add(setting, 'ObjData');
+    }
+
+    initGroupData(gui) {
+        const GroupData = () => {
+            console.log('---------------');
+            console.log('groupcode data: ', this.mapData.dataGroupCode.getCodes()); // 현재 맵의 group code data를 반환합니다.
+            console.log('하위 group code: ', this.mapData.dataGroupCode.findChild('A1')); // 1. 입력받은 code의 하위 group code들 반환
+            console.log('모든 하위 group code: ', this.mapData.dataGroupCode.findAllChild('A1')); // 2. 입력받은 code의 모든 하위 group code들 반환
+            console.log('부모 group code: ', this.mapData.dataGroupCode.findParent('A1-1')); // 3. 입력받은 code의 부모 group code 반환
+            console.log('최상위 group code: ', this.mapData.dataGroupCode.findRootParent('A1-1')); // 4. 입력받은 code의 최상위 부모 group code 반환
+            console.log('모든 root code: ', this.mapData.dataGroupCode.findAllRoot()); // 5. 현재 맵의 모든 root parent들을 반환합니다.
+            console.log('현재 지도의 모든 group code list: ', this.mapData.dataGroupCode.findAll()); // 6. 현재 맵의 모든 code를 반환합니다.
+            console.log('---------------');
+        };
+        const setting = {
+            GroupData: GroupData,
+        };
+        gui.add(setting, 'GroupData');
     }
 }
